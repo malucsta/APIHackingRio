a41b4166c58cf343bbb90ba6da44fd3d6b7340fd
Maria Luísa <araujocostamalu@gmail.com>
2022-09-03 22:53:27 -0300

    chore: removing db related files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,9 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import config from '../ormconfig';
 import { AirConditionerModule } from './air-conditioner/air-conditioner.module';
 import { WaterResourceModule } from './water-resource/water-resources.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot(config),
-    AirConditionerModule,
-    WaterResourceModule,
-  ],
+  imports: [AirConditionerModule, WaterResourceModule],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,7 @@ async function bootstrap() {
     )
     .setVersion('1.0')
     .build();
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
